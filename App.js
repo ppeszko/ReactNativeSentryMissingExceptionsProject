9c9c4ef41fa53dfc0855e84237583fa00ce06aac
Patryk Peszko <patryk@brainsandbeards.com>
2019-01-25 13:33:55 +0100

    Throw an exception which will be never send to sentry

--- App.js
+++ App.js
@@ -9,6 +9,11 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config('https://51e610c7477a4363af4ccfd9517e0bdb@sentry.io/1352206').install();
+
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -19,6 +24,7 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    throw new Error('This will never be send to Sentry');
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
